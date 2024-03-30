2d14577294a94f50cfba03eb5b3a55e7bbc6e72f
Suhas H C <suhashc193@gmail.com>
2024-03-30 21:32:45 +0530

    Refactored imports and re-arranged it

--- src/tests/UserDropDown.test.js
+++ src/tests/UserDropDown.test.js
@@ -1,5 +1,5 @@
+import { render, screen } from "@testing-library/react";
 import React from "react";
-import { screen, render } from "@testing-library/react";
 import UserDropDown from "../component/UserDropDown";
 
 describe("Testing the User Dropdown component", () => {
