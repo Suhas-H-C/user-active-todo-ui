0fd858e0f944db40606da1e4689a36376aade389
Suhas H C <suhashc193@gmail.com>
2024-07-14 13:54:22 +0530

    service test added

---
+++ src/tests/service/DropDownService.test.js
@@ -0,0 +1,21 @@
+import { URL } from "../../constant/Constant";
+import getUsers from "../../service/DropDownService";
+import { users } from "../utils/TestUtils";
+
+describe("Tests for Dropdown service", () => {
+  beforeEach(() => {
+    global.fetch = jest.fn();
+  });
+
+  it("should call users data detail API", async () => {
+    global.fetch.mockResolvedValueOnce({
+      ok: true,
+      json: jest.fn().mockResolvedValue({ users }),
+    });
+
+    let response = await getUsers(URL);
+    let data = await response.json();
+    expect(fetch).toHaveBeenCalledWith(URL);
+    expect(data.users).toHaveLength(1);
+  });
+});
