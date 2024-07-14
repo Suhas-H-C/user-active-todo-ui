384efe8704945c6f7acdb060ef7210406466ffaa
Suhas H C <suhashc193@gmail.com>
2024-07-14 13:37:41 +0530

    removed unwanted lines

--- src/context/UserDropDownContextProvider.js
+++ src/context/UserDropDownContextProvider.js
@@ -10,12 +10,8 @@ const UserDropDownContextProvider = ({ children }) => {
 
   const fetchUserDetails = async () => {
     try {
-      const response = await getUsers(URL);
-      if (!response.ok) {
-        throw new Error("Network response was not ok");
-      }
-      const users = await response.json();
-      const names = users.map((user) => user.name);
+      const response = await getUsers(URL).json();
+      const names = response.map((user) => user.name);
       setState((prev) => ({ ...prev, username: names }));
     } catch (error) {
       console.error("Failed fetching users data:", error);
