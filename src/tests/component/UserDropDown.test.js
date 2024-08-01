b88930f9aa6b40687eb91f6ab9f36cf1915061be
Suhas H C <suhashc193@gmail.com>
2024-08-01 13:02:34 +0530

    tests added with configuration

--- src/tests/component/UserDropDown.test.js
+++ src/tests/component/UserDropDown.test.js
@@ -1,9 +1,9 @@
 import { act, render, screen, waitFor } from "@testing-library/react";
 import React from "react";
-import UserDropDown from "../component/UserDropDown";
-import UserDropDownContextProvider from "../context/UserDropDownContextProvider";
-import getUsers from "../service/DropDownService";
-import { users } from "./utils/TestUtils";
+import UserDropDown from "../../component/UserDropDown";
+import UserDropDownContextProvider from "../../context/UserDropDownContextProvider";
+import getUsers from "../../service/DropDownService";
+import { users } from "../utils/TestUtils";
 
 describe("Tests for User Dropdown component", () => {
   getUsers = jest.fn().mockResolvedValue({
