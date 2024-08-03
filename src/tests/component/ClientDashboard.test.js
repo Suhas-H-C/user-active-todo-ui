94fa82d53c98267e09f871ea1e4eda38187af233
Suhas H C <suhashc193@gmail.com>
2024-08-03 11:14:17 +0530

    Added tests for Client dashboard

--- src/tests/component/ClientDashboard.test.js
+++ src/tests/component/ClientDashboard.test.js
@@ -1,12 +1,15 @@
 import "@testing-library/jest-dom";
-import { render, screen } from "@testing-library/react";
-import React from "react";
+import { render, screen, waitFor } from "@testing-library/react";
+import React, { act } from "react";
 import ClientDashboard from "../../component/ClientDashboard";
-import { UserDropDownContext } from "../../context/UserDropDownContextProvider";
-import { CLIENT_DASHBOARD_CONTEXT } from "../utils/TestUtils";
+import UserDropDownContextProvider, {
+  UserDropDownContext,
+} from "../../context/UserDropDownContextProvider";
+import * as ApiConfig from "../../service/ApiFetchConfig";
+import { CLIENT_DASHBOARD_CONTEXT, todos } from "../utils/TestUtils";
 
 describe("Tests for Client Dashboard component", () => {
-  it("should render client component with greetings for provided name", async () => {
+  it("should render client component with greetings for provided name with data grid", async () => {
     render(
       <UserDropDownContext.Provider value={CLIENT_DASHBOARD_CONTEXT}>
         <ClientDashboard name={CLIENT_DASHBOARD_CONTEXT.names} />
@@ -14,7 +17,44 @@ describe("Tests for Client Dashboard component", () => {
     );
 
     const message = screen.getByText(/Welcome John and others/i);
+    const userId = screen.getByText("User ID");
+    const Id = screen.getByText("ID");
+    const title = screen.getByText("Title");
+    const completed = screen.getByText("Completed");
+
     expect(message).toBeInTheDocument();
     expect(CLIENT_DASHBOARD_CONTEXT.fetchGridData).toHaveBeenCalled();
+    expect(userId).toBeInTheDocument();
+    expect(Id).toBeInTheDocument();
+    expect(title).toBeInTheDocument();
+    expect(completed).toBeInTheDocument();
+  });
+
+  it("user should see a row on the data grid when component is rendered", async () => {
+    ApiConfig.fetchResponse = jest.fn().mockResolvedValue({
+      data: todos,
+    });
+
+    await act(async () => {
+      render(
+        <UserDropDownContextProvider>
+          <ClientDashboard name={CLIENT_DASHBOARD_CONTEXT.names} />
+        </UserDropDownContextProvider>
+      );
+    });
+
+    await waitFor(() => {
+      const title = screen.getByRole("gridcell", {
+        name: /delectus aut autem/i,
+      });
+      expect(title).toBeInTheDocument();
+    });
+
+    await waitFor(() => {
+      const completed = screen.getByRole("gridcell", {
+        name: /false/i,
+      });
+      expect(completed).toBeInTheDocument();
+    });
   });
 });
